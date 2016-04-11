f8d3a3a688e72a20094d5bc69ff2e20e551b37d8
Steve Fake <steven.m.fake@gmail.com>
2016-04-11 13:25:04 -0400

    add rails files

---
+++ app/controllers/concerns/WelcomeController.rb
@@ -0,0 +1,32 @@
+class ApplicationController < ApplicationController
+  # def index
+  # # Index: GET "/", returns a welcome message
+  # do
+  #   "Welcome!!"
+  # end
+  #
+  # def name
+  # # Show: GET "/billy", returns a specializaed welcome message for whatever name is passed to it as a param
+  # do
+  #   "Welcome #{:name}"
+  # end
+  #
+  # def lorem
+  # # Lorem: GET "/lorem/standard", returns lorem ipsum of the type requested
+  # do
+  #   "<%= http://meettheipsums.com/  %>"
+
+end
+
+# class PostsController < ApplicationController
+#   def index
+#     @posts = Post.all
+#   end
+#
+#   def new
+#   end
+#
+#   def edit
+#     @post = Post.find(params["id"])
+#   end
+# end
